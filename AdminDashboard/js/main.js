5fa9246fd36dff32f65dc3b5c9fe955936391e7a
ahsbkdsilva <96135295+ahsbkdsilva@users.noreply.github.com>
2022-10-06 01:01:40 +0530

    js editing

--- AdminDashboard/js/main.js
+++ AdminDashboard/js/main.js
@@ -60,7 +60,7 @@
 
 
     // Chart Global Color
-    Chart.defaults.color = "#6C7293";
+    Chart.defaults.color = "#fff";
     Chart.defaults.borderColor = "#000000";
 
 
@@ -69,21 +69,26 @@
     var myChart1 = new Chart(ctx1, {
         type: "bar",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["3'Oct", "4'Oct", "5'Oct", "6'Oct", "7'Oct", "8'Oct", "9'Oct" ],
             datasets: [{
-                    label: "USA",
+                    label: "CA",
                     data: [15, 30, 55, 65, 60, 80, 95],
-                    backgroundColor: "rgba(235, 22, 22, .7)"
+                    backgroundColor: "rgba(0, 250, 154, .7)"
                 },
                 {
-                    label: "UK",
+                    label: "JP",
                     data: [8, 35, 40, 60, 70, 55, 75],
-                    backgroundColor: "rgba(235, 22, 22, .5)"
+                    backgroundColor: "rgba(240, 128, 128, .5)"
                 },
                 {
                     label: "AU",
                     data: [12, 25, 45, 55, 65, 70, 60],
-                    backgroundColor: "rgba(235, 22, 22, .3)"
+                    backgroundColor: "rgba(32, 178, 170, .3)"
+                },
+                {
+                    label: "Other",
+                    data: [12, 25, 45, 55, 65, 70, 60],
+                    backgroundColor: "rgba(173, 255, 47, .4)"
                 }
             ]
             },
@@ -98,17 +103,17 @@
     var myChart2 = new Chart(ctx2, {
         type: "line",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["3'Oct", "4'Oct", "5'Oct", "6'Oct", "7'Oct", "8'Oct", "9'Oct"],
             datasets: [{
-                    label: "Salse",
+                    label: "Arrivals",
                     data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    backgroundColor: "rgba(221, 160, 221, .7)",
                     fill: true
                 },
                 {
-                    label: "Revenue",
+                    label: "Depatures",
                     data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(235, 22, 22, .5)",
+                    backgroundColor: "rgba(95, 158, 160, .5)",
                     fill: true
                 }
             ]
