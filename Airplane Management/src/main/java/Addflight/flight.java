849c59bc15bb6c3eab413ef018dafb58e9df8e1a
kaveesha jayakody <98570088+kaveeshajayakody@users.noreply.github.com>
2022-10-07 15:18:42 +0530

    *

---
+++ Airplane Management/src/main/java/Addflight/flight.java
@@ -0,0 +1,45 @@
+package Addflight;
+
+public class Flights2 {
+	private int FLIGHTID;
+	private String FROM;
+	private String DESTINATION;
+	private String PLANETYPE;
+	private String AVAILABLESEATS;
+	private String DATE;
+	private String TIME;
+	
+	public Flights2(int fLIGHTID, String fROM, String dESTINATION, String pLANETYPE, String aVAILABLESEATS, String dATE,String tIME) 
+	{
+		
+		FLIGHTID = flightid;
+		START = start;
+		END = END;
+		NO_SEATS = no_seats;
+		PILOT = pilot;
+		
+	}
+
+	public int getFLIGHTID() {
+		return FLIGHTID;
+	}
+
+	public String getSTART() {
+		return START;
+	}
+
+	public String getEND() {
+		return END;
+	}
+
+
+	public String getNO_SEATS() {
+		return NO_SEATS;
+	}
+
+	public String getPILOT() {
+		return PILOT;
+	}
+
+	
+} 
