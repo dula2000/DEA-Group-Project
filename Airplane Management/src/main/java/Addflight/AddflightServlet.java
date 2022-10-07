78168ca94ddc1cef747ece6eb6455c15a177275a
kaveesha jayakody <98570088+kaveeshajayakody@users.noreply.github.com>
2022-10-07 11:21:24 +0530

    Add files via upload
    addflight

---
+++ Airplane Management/src/main/java/Addflight/AddflightServlet.java
@@ -0,0 +1,61 @@
+package com.flights2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@jakarta.servlet.annotation.WebServlet("/FlightInsertServ2")
+public class FlightInsertServ2 extends HttpServlet {
+    
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+		
+		//Getting Parametars from staff.jsp......
+		PrintWriter out = response.getWriter();
+		
+		Integer FLIGHT_ID = Integer.parseInt(request.getParameter("Fid"));
+		
+		String  STARTS = request.getParameter("start");
+		
+		String  END = request.getParameter("end");
+		
+		String  NO_SEATS = request.getParameter("ns");
+		
+		String  PILOT = request.getParameter("plt")
+		
+
+		//calling insertcustomer() Methode
+				boolean isTrue;
+				
+				isTrue = FlightDBUtil2.insertflights(FLIGHTID,FROM,DESTINATION,PLANETYPE,AVAILABLESEATS,DATE,TIME);
+				
+				if(isTrue == true) {
+					out.println("<script type = 'text/javascript'>");
+					out.println("alert('Flight Details Successfully Added');");
+					out.println("location = 'flight.jsp'");
+					out.println("</script>");
+				} else {
+					RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+					dis.forward(request, response);
+				}
+	
+		}
+		
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+   }	
+
+}
